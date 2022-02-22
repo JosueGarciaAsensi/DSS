fc46a237f10d4b696305f4303476eb5c9c58b67b
Jordi Sellés Enríquez <jse20@alu.ua.es>
2022-02-22 22:52:36 +0100

    cesta ok

---
+++ tabernadelchino/database/migrations/2022_02_22_214756_create_cesta_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cesta', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID('id_pedido')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cesta');
+    }
+};
