8ec8e67c082b1541d1a9e82fa46e79b5cb6fe8b2
josuejx <josue.ga00@gmail.com>
2022-03-25 18:53:38 +0100

    Instalación Bootstrap y página layoutterminada

--- tabernadelchino/resources/views/layout.blade.php
+++ tabernadelchino/resources/views/layout.blade.php
@@ -1,38 +1,45 @@
 <html>
     <head>
         <title>@yield('title')</title>
-        <script src="{{ asset('js/bootstrap.js') }}" defer></script>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </head>
     <body>
         @section('menu')
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-                <a href="#" class="navbar-brand">Brand</a>
+                <img src="{{ asset('img/logo.png') }}" width=80>
+                <a href="#" class="navbar-brand">La Taberna del Chino</a>
                 <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
                     <div class="navbar-nav">
-                        <a href="#" class="nav-item nav-link active">Home</a>
-                        <a href="#" class="nav-item nav-link">Profile</a>
-                        <div class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Messages</a>
-                            <div class="dropdown-menu">
-                                <a href="#" class="dropdown-item">Inbox</a>
-                                <a href="#" class="dropdown-item">Sent</a>
-                                <a href="#" class="dropdown-item">Drafts</a>
-                            </div>
-                        </div>
+                        <a href="#" class="nav-item nav-link active">Inicio</a>
+                        <a href="#" class="nav-item nav-link">Productos</a>
+                        <a href="#" class="nav-item nav-link">Sobre nosotros</a>
                     </div>
                     <form class="d-flex">
-                        <div class="input-group">                    
+                        <div class="input-group mt-3">                    
                             <input type="text" class="form-control" placeholder="Search">
-                            <button type="button" class="btn btn-secondary"><i class="bi-search"></i></button>
+                            <button type="button" class="btn btn-secondary">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search text-light" viewBox="0 0 16 16">
+                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                </svg>
+                            </button>
                         </div>
                     </form>
                     <div class="navbar-nav">
-                        <a href="#" class="nav-item nav-link">Login</a>
+                        <a href="#" class="nav-item nav-link">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bag text-light mt-2" viewBox="0 0 16 16">
+                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+                            </svg>
+                        </a>
+                        <a href="#" class="nav-item nav-link">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-person text-light mt-2" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                        </svg>
+                        </a>
                     </div>
                 </div>
             </div>
