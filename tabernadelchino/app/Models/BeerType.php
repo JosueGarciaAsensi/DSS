2a7abe2b813f926f0dcd8651f5854cd93b5b43e6
ifranferrandez <ffm18@alu.ua.es>
2022-03-26 17:05:08 +0100

    Model BeerType implemented

--- tabernadelchino/app/Models/BeerType.php
+++ tabernadelchino/app/Models/BeerType.php
@@ -10,4 +10,38 @@ class BeerType extends Model
     use HasFactory;
 
     public function product() { return $this->hasMany('App\Models\Product'); }
+
+    /** 
+    *   Problema: ¿La clase es como una factoría 
+    *    o trabajo sobre mis objetos? 
+    */
+    public function create($name, $visible) {
+        $beer_type = new BeerType([$name, $visible]);
+        $beer_type->save();
+    }
+
+    public function read($name) {
+        return BeerType::findOrFail($name);
+    }
+
+    public function edit($new_name, $visible) {
+        $this->name = $new_name;
+        $this->visible = $visible;
+
+        $this->save();
+    }
+
+    public function remove() {
+        $this->delete();
+    }
+
+    public function toggleVisible() {
+        if ($this->visible == true) {
+            $this->visible = false;
+        } else {
+            $this->visble = true;
+        }
+
+        $this->save();
+    }
 }
