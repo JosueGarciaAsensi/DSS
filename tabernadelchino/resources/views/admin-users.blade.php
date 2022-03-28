43b07b17bf69a8bba0f5ebf8fa3a0dc789a584f0
David Pastor Crespo <davidgasol913@gmail.com>
2022-03-28 18:57:39 +0200

    Grid admin-users terminado (estilos mal)

--- tabernadelchino/resources/views/admin-users.blade.php
+++ tabernadelchino/resources/views/admin-users.blade.php
@@ -6,16 +6,23 @@
 
 @section('content')
 <div class="container mt-5 mb-5 rounded" style="background-color: black;">
-</div>
-
-<div class="container">
-    @foreach ($users as $user)
-    <div class="row row-cols-4" style="background-color: white;">
-        <div class="col">Col</div>
-        <div class="col">Col</div>
-        <div class="col">Col</div>
-        <div class="col">Col</div>            
+    <div class="container">
+        <div class="row row-cols-5" style="text-align: center; color: white;">
+            <div class="col"><b>Nombre</b></div>
+            <div class="col"><b>Apellidos</b></div>
+            <div class="col"><b>Email</b></div>
+            <div class="col"><b>DNI</b></div>
+            <div class="col"><b>Admin</b></div>
+        </div>
+        <div class="row row-cols-5" style="text-align: center; color: white;">
+            @foreach ($users as $user)
+            <div class="col">{{$user->name}}</div>
+            <div class="col">{{$user->surname}}</div>
+            <div class="col">{{$user->email}}</div>
+            <div class="col">{{$user->dni}}</div>
+            <div class="col">{{$user->admin}}</div>
+            @endforeach
+        </div>
     </div>
-    @endforeach
 </div>
 @endsection
