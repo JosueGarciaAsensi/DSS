811ebbdf2113a44bc41fbb69f13bdd14916eb28b
Jordi Sellés Enríquez <jse20@alu.ua.es>
2022-03-28 19:19:33 +0200

    pills

--- tabernadelchino/resources/views/admin-menu.blade.php
+++ tabernadelchino/resources/views/admin-menu.blade.php
@@ -6,21 +6,21 @@
 @section('content')
     <div class="container rounded mt-5 mb-5 p-3" style="background-color:black;">
         <div class="row mb-3">
-            <div class="col" style="background-color:cadetblue;">
+            <div class="col rounded-pill px-4 mx-2" style="background-color:cadetblue;">
                 <h3>{{ $usersCount }}</h3>
                 <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-person text-dark mt-2" viewBox="0 0 16 16">
                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                 </svg>
             </div>
-            <div class="col" style="background-color:crimson;">
+            <div class="col rounded-pill px-4 mx-2" style="background-color:crimson;">
                 <h3>{{ $productsCount }}</h3>
             </div>
-            <div class="col" style="background-color:blueviolet;">
+            <div class="col rounded-pill px-4 mx-2" style="background-color:blueviolet;">
                 <h3>{{ $ordersCount }}</h3>
             </div>
         </div>
         <div class="row">
-            <div class="col" style="background-color:darkgreen;">
+            <div class="col rounded-pill px-4 mx-2" style="background-color:darkgreen;">
                 <h3>{{ $ordersCount/$usersCount }}</h3>
             </div>
         </div>
