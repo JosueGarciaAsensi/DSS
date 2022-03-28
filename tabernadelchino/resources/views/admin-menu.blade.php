8715fd6abb4a12e5864d3958f01872d64c037808
Jordi Sellés Enríquez <jse20@alu.ua.es>
2022-03-28 22:56:17 +0200

    igualando pills

--- tabernadelchino/resources/views/admin-menu.blade.php
+++ tabernadelchino/resources/views/admin-menu.blade.php
@@ -19,15 +19,43 @@
                 </div>
             </div>
             <div class="col rounded-pill px-4 mx-2" style="background-color:crimson;">
-                <h3>{{ $productsCount }}</h3>
+                <div class="row">
+                    <div class="col">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-person text-dark mt-2" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                        </svg>
+                    </div>
+                    <div class="col d-flex justify-content-end">
+                        <h3>{{ $productsCount }}</h3>
+                    </div>
+                </div>
+
             </div>
             <div class="col rounded-pill px-4 mx-2" style="background-color:blueviolet;">
-                <h3>{{ $ordersCount }}</h3>
+                <div class="row">
+                    <div class="col">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-person text-dark mt-2" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                        </svg>
+                    </div>
+                    <div class="col d-flex justify-content-end">
+                        <h3>{{ $ordersCount }}</h3>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="row">
             <div class="col rounded-pill px-4 mx-2" style="background-color:darkgreen;">
-                <h3>{{ $ordersCount/$usersCount }}</h3>
+                <div class="row">
+                    <div class="col">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-person text-dark mt-2" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                        </svg>
+                    </div>
+                    <div class="col d-flex justify-content-end">
+                        <h3>{{ $ordersCount/$usersCount }}</h3>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
