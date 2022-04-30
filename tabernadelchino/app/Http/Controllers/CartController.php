71cd9816e7f3e1a55944c5ab50f3be5ae94c0cb2
Jordi Sellés Enríquez <jse20@alu.ua.es>
2022-04-30 18:29:42 +0200

    Cart Controller

--- tabernadelchino/app/Http/Controllers/CartController.php
+++ tabernadelchino/app/Http/Controllers/CartController.php
@@ -33,7 +33,7 @@ class CartController extends Controller
 
         $cart->products()->attach($product);
 
-        return back();
+        return redirect()->back();
     }
 
     public function removeFromCart($id, $idItem) {
