8bf1f35cc1b04a44c249f28c95db6008928fe98a
JosueGarcia Asensi <jga74@alu.ua.es>
2022-05-08 12:16:58 +0200

    Filtro busqueda arreglado

--- tabernadelchino/resources/views/products.blade.php
+++ tabernadelchino/resources/views/products.blade.php
@@ -30,31 +30,30 @@
 
                     <h3 class="text-light d-flex justify-content-center">
                         <div class="mx-4">{{ $product->price; }}€</div>
-                        @if($product->stock > 0 && $product->stock < 5)
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-circle-fill text-warning align-self-center" viewBox="0 0 16 16">
+                        @if($product->stock > 0 && $product->stock < 5) <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-circle-fill text-warning align-self-center" viewBox="0 0 16 16">
                             <circle cx="8" cy="8" r="8" />
-                        </svg>
-                        @elseif($product->stock > 0)
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-circle-fill text-success align-self-center" viewBox="0 0 16 16">
-                            <circle cx="8" cy="8" r="8" />
-                        </svg>
-                        @else
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-circle-fill text-danger align-self-center" viewBox="0 0 16 16">
-                            <circle cx="8" cy="8" r="8" />
-                        </svg>
-                        @endif
+                            </svg>
+                            @elseif($product->stock > 0)
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-circle-fill text-success align-self-center" viewBox="0 0 16 16">
+                                <circle cx="8" cy="8" r="8" />
+                            </svg>
+                            @else
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-circle-fill text-danger align-self-center" viewBox="0 0 16 16">
+                                <circle cx="8" cy="8" r="8" />
+                            </svg>
+                            @endif
+
                     </h3>
+                </td>
+                @php($i = $i + 1)
+                @if( $i == 4)
+            </tr>
+            @php($i = 1)
+            @endif
+            @endforeach
+        </table>
+        @endif
     </div>
-    </td>
-    @php($i = $i + 1)
-    @if( $i == 4)
-    </tr>
-    @php($i = 1)
-    @endif
-    @endforeach
-    </table>
-    @endif
-</div>
-<div class="d-flex justify-content-center">{{ $products->links() }}</div>
+    <div class="d-flex justify-content-center">{{ $products->appends(request()->except('page'))->links() }}</div>
 </div>
 @endsection
